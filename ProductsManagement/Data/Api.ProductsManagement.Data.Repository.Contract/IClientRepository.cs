6889b8411233fbdd25d8a769313474217cddbf14
jb-mddln <13810686+jb-mddln@users.noreply.github.com>
2023-06-29 13:16:49 +0200

    Delete IClientRepository.cs

--- ProductsManagement/Data/Api.ProductsManagement.Data.Repository.Contract/IClientRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Api.ProductsManagement.Data.Entity.Model;
-
-namespace Api.ProductsManagement.Data.Repository.Contract
-{
-    public interface IClientRepository : IRepository<Client>
-    {
-        // Task<IEnumerable<Client>> GetClientsAsync();
-    }
-}
